2d3ca216ceebc0209b31cb6b45f8fcccc9df6b26
wlsdud15255 <wlsdud15255@gmail.com>
2023-08-01 16:33:55 +0900

    최종 수정본

--- lendingpage/main.js
+++ lendingpage/main.js
@@ -10,24 +10,41 @@ setInterval(blink, 500);
 
 // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
-let stringArr = ["learn to html", "learn to css", "learn to javascript"];
-let selectStr = stringArr[Math.floor(Math.random() * stringArr.length)];
-// 5. js의 math 내장함수
-// let selectStr = stringArr[Math.random() * 3];
+function randomString() {
+  let stringArr = ["learn to html", "learn to css", "learn to javascript"];
+  let selectStr = stringArr[Math.floor(Math.random() * stringArr.length)];
+  // 5. js의 math 내장함수
+  // let selectStr = stringArr[Math.random() * 3];
 
-let selectStrArr = selectStr.split("");
+  let selectStrArr = selectStr.split("");
 
+  return selectStrArr;
+}
+
+// 타이핑 리셋
+function resetTyping() {
+  target.textContent = "";
+
+  dynamic(randomString());
+}
+
+// 한글자씩 텍스트 출력 함수
 function dynamic(randomArr) {
   if (randomArr.length > 0) {
     // 6. shift 내장함수
     target.textContent += randomArr.shift();
     // 7. settimeout 내장함수
+
+    // 8. setTimeout(function () {dynamic(randomArr);}, 80); 와
+    //    setTimeout(dynamic(randomArr);, 80);
     setTimeout(function () {
       dynamic(randomArr);
     }, 80);
+  } else {
+    setTimeout(resetTyping, 3000);
   }
 }
 
 // 28분
 
-dynamic(selectStrArr);
+dynamic(randomString());
