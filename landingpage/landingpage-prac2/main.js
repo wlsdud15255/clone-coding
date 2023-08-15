942aeff7d175eb0bb3f3574c9668539fe1ad2cf8
wlsdud15255 <wlsdud15255@gmail.com>
2023-08-15 16:42:33 +0900

    강의 안보고 복습

--- landingpage/landingpage-prac2/main.js
+++ landingpage/landingpage-prac2/main.js
@@ -1,32 +1,40 @@
-let iii = document.querySelector("#dynamic");
+let a = document.querySelector("#Typing");
+
+// blink
+// 해당 요소에 active 클래스를 부여했다 없애는 함수
 
 let blink = () => {
-  iii.classList.toggle("active");
+  a.classList.toggle("active");
 };
 
 setInterval(blink, 500);
 
-// ----------
+//
 
-let str = "...";
-let ArrStr = str.split("");
+function randomStr() {
+  let learnArr = ["learn to HTML", "learn to CSS", "learn to JS"];
+  let oneStr = learnArr[Math.floor(Math.random() * learnArr.length)];
+  let strArr = oneStr.split("");
 
-function resetTyping() {
-  target.textContent = "";
-
-  typing(ArrStr);
+  return strArr;
 }
 
-let typing = (Arr) => {
-  if (Arr.length > 0) {
-    iii.textContent += Arr.shift();
+let Typing = (selectStr) => {
+  if (selectStr.length > 0) {
+    a.textContent += selectStr.shift();
 
     setTimeout(function () {
-      typing(Arr);
-    }, 500);
+      Typing(selectStr);
+    }, 50);
   } else {
-    setTimeout(resetTyping, 3000);
+    setTimeout(reset, 3000);
   }
 };
 
-typing(ArrStr);
+let reset = () => {
+  a.textContent = "";
+
+  Typing(randomStr());
+};
+
+Typing(randomStr());
